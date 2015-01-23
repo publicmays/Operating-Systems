33537c88408795058113bd1fe81f91465a703c64
katlew3 <katlew3@users.noreply.github.com>
2015-01-23 16:38:11 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -15,3 +15,28 @@ struct qentry {             /* 1 per process + 2 per list */
   qid16 qnext;              /* index of next process or tail */
   qid16 qprev;              /* index of prev process or head */
 };
+
+extern struct qentry queuetab[];  // array of qentry structures
+
+/* Inline queueu manipulation functions */
+#define queuehead(q)  (q)
+#define queuetail(q)  ((q) + 1)
+#define firstid(q)    (queuetab[queuehead(q)].qnext)
+#define lastid(q)     (queuetab[queuetail(q)].qprev)
+#define isempty(q)    (firstid(q) >= NPROC)
+#define nonempty(q)   (firstid(q) < NPROC)
+#define firstkey(q)   (queuetab[firstid(q)].qkey)
+#define lastkey(q)    (queuetab[lastid(q)].qkey)
+
+/* Inline to check queue id assumes interrupts are disabled */
+#define isbadqid(x)   (((int32)(x) < 0) || (int32)(x) >= NQENT-1)
+
+/* Queue function prototypes */
+pid32 getfirst(qid16);
+pid32 getlast(qid16);
+pid32 getitem(pid32);
+pid32 enqueue(pid32, qid16);
+pid32 dequeue(qid16);
+status insert(pid32, qid16, int);
+status insertd(pid32, qid16, int);
+qid16 newqueue(void);
